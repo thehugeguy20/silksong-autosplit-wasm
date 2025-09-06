bf615e5cd0caee34c3952df491a8c4a7735362c5
AlexKnauth <alexander@knauth.org>
2025-09-06 16:38:03 -0400

    Start on load remover and hit counter

--- src/lib.rs
+++ src/lib.rs
@@ -7,23 +7,56 @@ static ALLOC: dlmalloc::GlobalDlmalloc = dlmalloc::GlobalDlmalloc;
 mod silksong_memory;
 mod unstable;
 
-use alloc::string::{String, ToString};
+use alloc::{boxed::Box, format};
 use asr::{
     future::{next_tick, retry},
     settings::Gui,
     timer::TimerState,
-    Process,
+    Address64, Process,
 };
 
-use crate::silksong_memory::attach_silksong;
+use crate::silksong_memory::{
+    attach_silksong, GameManagerPointers, Memory, PlayerDataPointers, MENU_TITLE, QUIT_TO_MENU,
+};
 
 asr::async_main!(stable);
 asr::panic_handler!();
 
+// --------------------------------------------------------
+
+pub const GAME_STATE_INACTIVE: i32 = 0;
+pub const GAME_STATE_MAIN_MENU: i32 = 1;
+pub const GAME_STATE_LOADING: i32 = 2;
+pub const GAME_STATE_ENTERING_LEVEL: i32 = 3;
+pub const GAME_STATE_PLAYING: i32 = 4;
+// pub const GAME_STATE_PAUSED: i32 = 5;
+pub const GAME_STATE_EXITING_LEVEL: i32 = 6;
+pub const GAME_STATE_CUTSCENE: i32 = 7;
+
+// UI_STATE 1: Main Menu
+pub const UI_STATE_PLAYING: i32 = 4;
+pub const UI_STATE_PAUSED: i32 = 5;
+
+// HERO_TRANSITION_STATE 0: N/A, not in transition
+// HERO_TRANSITION_STATE 1: Exiting scene
+// HERO_TRANSITION_STATE 2, 3: Waiting to enter, Entering?
+pub const HERO_TRANSITION_STATE_WAITING_TO_ENTER_LEVEL: i32 = 2;
+
+// --------------------------------------------------------
+
 struct AutoSplitterState {
     timer_state: TimerState,
     split_index: Option<u64>,
-    events: String,
+    look_for_teleporting: bool,
+    last_ui_state: i32,
+    last_game_state: i32,
+    last_hero_transition_state: i32,
+    hits: i64,
+    last_recoil: bool,
+    last_hazard: bool,
+    last_health_0: bool,
+    last_health: Option<i32>,
+    last_paused: bool,
 }
 
 impl AutoSplitterState {
@@ -33,7 +66,16 @@ impl AutoSplitterState {
         AutoSplitterState {
             timer_state,
             split_index,
-            events: "".to_string(),
+            look_for_teleporting: false,
+            last_ui_state: 0,
+            last_game_state: GAME_STATE_INACTIVE,
+            last_hero_transition_state: 0,
+            hits: 0,
+            last_recoil: false,
+            last_hazard: false,
+            last_health_0: false,
+            last_health: None,
+            last_paused: false,
         }
     }
 
@@ -45,54 +87,37 @@ impl AutoSplitterState {
         }
 
         match new_state {
-            TimerState::NotRunning if self.timer_state == TimerState::Running || self.timer_state == TimerState::Paused || self.timer_state == TimerState::Ended => {
-                self.events += "3";
-                asr::timer::set_variable("events", str_take_right(&self.events, 10));
-                asr::timer::set_variable("last", "Reset");
-                asr::print_message("Reset.");
+            TimerState::NotRunning
+                if self.timer_state == TimerState::Running
+                    || self.timer_state == TimerState::Paused
+                    || self.timer_state == TimerState::Ended =>
+            {
+                // Reset
             }
             TimerState::Running if is_timer_state_between_runs(self.timer_state) => {
-                self.events += "1";
-                asr::timer::set_variable("events", str_take_right(&self.events, 10));
-                asr::timer::set_variable("last", "Start");
-                asr::print_message("Started.");
+                // Start
             }
             TimerState::Paused if self.timer_state == TimerState::Running => {
-                self.events += "5";
-                asr::timer::set_variable("events", str_take_right(&self.events, 10));
-                asr::timer::set_variable("last", "Pause");
-                asr::print_message("Paused.");
+                // Pause
             }
             TimerState::Running if self.timer_state == TimerState::Paused => {
-                self.events += "5";
-                asr::timer::set_variable("events", str_take_right(&self.events, 10));
-                asr::timer::set_variable("last", "Resume");
-                asr::print_message("Resumed.");
+                // Resume
             }
-            TimerState::Ended if self.timer_state == TimerState::Running || self.timer_state == TimerState::Paused => {
-                self.events += "1";
-                asr::timer::set_variable("events", str_take_right(&self.events, 10));
-                asr::timer::set_variable("last", "End");
-                asr::print_message("Ended.");
+            TimerState::Ended
+                if self.timer_state == TimerState::Running
+                    || self.timer_state == TimerState::Paused =>
+            {
+                // End
             }
             _ => {
                 if let (Some(new_index), Some(old_index)) = (&new_index, &self.split_index) {
                     if new_index < old_index {
-                        self.events += "8";
-                        asr::timer::set_variable("events", str_take_right(&self.events, 10));
-                        asr::timer::set_variable("last", "Undo");
-                        asr::print_message("Undid.");
+                        // Undo
                     } else if new_index > old_index {
                         if unstable::timer_segment_splitted(new_index - 1).unwrap_or_default() {
-                            self.events += "1";
-                            asr::timer::set_variable("events", str_take_right(&self.events, 10));
-                            asr::timer::set_variable("last", "Split");
-                            asr::print_message("Splitted.");
+                            // Split
                         } else {
-                            self.events += "2";
-                            asr::timer::set_variable("events", str_take_right(&self.events, 10));
-                            asr::timer::set_variable("last", "Skip");
-                            asr::print_message("Skipped.");
+                            // Skip
                         }
                     }
                 }
@@ -106,9 +131,9 @@ impl AutoSplitterState {
 
 #[derive(Gui)]
 struct Settings {
-    /// My Setting
+    /// Hit Counter
     #[default = true]
-    my_setting: bool,
+    hit_counter: bool,
     // TODO: Change these settings.
 }
 
@@ -117,6 +142,9 @@ async fn main() {
     let mut settings = Settings::register();
 
     asr::print_message("Hello, World!");
+
+    // register the variables on start
+    asr::timer::set_variable_int("hits", 0);
 
     let mut state = AutoSplitterState::new();
 
@@ -127,11 +155,28 @@ async fn main() {
         process
             .until_closes(async {
                 // TODO: Load some initial information from the process.
+                let mem = Memory::wait_attach(&process).await;
+                let gm = Box::new(GameManagerPointers::new());
+                let pd = Box::new(PlayerDataPointers::new());
+                let _: bool = mem.deref(&gm.accepting_input).unwrap_or_default();
+                let _: Address64 = mem.deref(&gm.entry_gate_name).unwrap_or_default();
+                let _: i32 = mem.deref(&gm.game_state).unwrap_or_default();
+                let _: bool = mem.deref(&gm.hazard_death).unwrap_or_default();
+                let _: bool = mem.deref(&gm.hazard_respawning).unwrap_or_default();
+                let _: bool = mem.deref(&gm.hero_recoil_frozen).unwrap_or_default();
+                let _: i32 = mem.deref(&gm.hero_transition_state).unwrap_or_default();
+                let _: Address64 = mem.deref(&gm.next_scene_name).unwrap_or_default();
+                let _: Address64 = mem.deref(&gm.scene_name).unwrap_or_default();
+                let _: i32 = mem.deref(&gm.ui_state_vanilla).unwrap_or_default();
+                let _: i32 = mem.deref(&pd.health).unwrap_or_default();
+                next_tick().await;
                 loop {
                     settings.update();
                     state.update();
 
                     // TODO: Do something on every tick.
+                    load_removal(&mut state, &mem, &gm);
+                    handle_hits(&mut state, &mem, &gm, &pd);
                     next_tick().await;
                 }
             })
@@ -139,16 +184,138 @@ async fn main() {
     }
 }
 
-async fn wait_attach_silksong(
-    gui: &mut Settings,
-    state: &mut AutoSplitterState,
-) -> Process {
+async fn wait_attach_silksong(gui: &mut Settings, state: &mut AutoSplitterState) -> Process {
     retry(|| {
         gui.update();
         state.update();
         attach_silksong()
     })
     .await
+}
+
+// --------------------------------------------------------
+
+fn load_removal(state: &mut AutoSplitterState, mem: &Memory, gm: &GameManagerPointers) {
+    // only remove loads if timer is running
+    if asr::timer::state() != TimerState::Running {
+        asr::timer::pause_game_time();
+        return;
+    }
+
+    let ui_state: i32 = mem.deref(&gm.ui_state_vanilla).unwrap_or_default();
+    let scene_name = mem.read_string(&gm.scene_name).unwrap_or_default();
+    let next_scene = mem.read_string(&gm.next_scene_name).unwrap_or_default();
+
+    let loading_menu = (scene_name != MENU_TITLE && next_scene.is_empty())
+        || (scene_name != MENU_TITLE && next_scene == MENU_TITLE || (scene_name == QUIT_TO_MENU));
+
+    // TODO: teleporting, look_for_teleporting
+
+    let game_state: i32 = mem.deref(&gm.game_state).unwrap_or_default();
+
+    if game_state == GAME_STATE_PLAYING && state.last_game_state == GAME_STATE_MAIN_MENU {
+        state.look_for_teleporting = true;
+    }
+    if state.look_for_teleporting
+        && (game_state != GAME_STATE_PLAYING && game_state != GAME_STATE_ENTERING_LEVEL)
+    {
+        state.look_for_teleporting = false;
+    }
+
+    // TODO: hazard_respawning
+    let accepting_input: bool = mem.deref(&gm.accepting_input).unwrap_or_default();
+    let hero_transition_state: i32 = mem.deref(&gm.hero_transition_state).unwrap_or_default();
+    // TODO: tile_map_dirty, uses_scene_transition_routine
+
+    let is_game_time_paused = (state.look_for_teleporting)
+        || ((game_state == GAME_STATE_PLAYING || game_state == GAME_STATE_ENTERING_LEVEL)
+            && ui_state != UI_STATE_PLAYING)
+        || (game_state != GAME_STATE_PLAYING && !accepting_input)
+        || (game_state == GAME_STATE_EXITING_LEVEL || game_state == GAME_STATE_LOADING)
+        || (hero_transition_state == HERO_TRANSITION_STATE_WAITING_TO_ENTER_LEVEL)
+        || (ui_state != UI_STATE_PLAYING
+            && (loading_menu
+                || (ui_state != UI_STATE_PAUSED
+                    && (!next_scene.is_empty() || scene_name == "_test_charms")))
+            && next_scene != scene_name);
+    if is_game_time_paused {
+        asr::timer::pause_game_time();
+    } else {
+        asr::timer::resume_game_time();
+    }
+
+    {
+        if ui_state != state.last_ui_state {
+            asr::print_message(&format!("ui_state: {}", ui_state));
+        }
+        state.last_ui_state = ui_state;
+    }
+
+    if game_state != state.last_game_state {
+        asr::print_message(&format!("game_state: {}", game_state));
+    }
+    state.last_game_state = game_state;
+
+    {
+        if hero_transition_state != state.last_hero_transition_state {
+            asr::print_message(&format!("hero_transition_state: {}", hero_transition_state));
+        }
+        state.last_hero_transition_state = hero_transition_state;
+    }
+
+    {
+        if is_game_time_paused != state.last_paused {
+            asr::print_message(&format!("is_game_time_paused: {}", is_game_time_paused));
+        }
+        state.last_paused = is_game_time_paused;
+    }
+}
+
+fn handle_hits(
+    state: &mut AutoSplitterState,
+    mem: &Memory,
+    gm: &GameManagerPointers,
+    pd: &PlayerDataPointers,
+) {
+    // only count hits if timer is running
+    if asr::timer::state() != TimerState::Running {
+        return;
+    }
+
+    let recoil: bool = mem.deref(&gm.hero_recoil_frozen).unwrap_or_default();
+    if !state.last_recoil && recoil {
+        add_hit(state);
+        asr::print_message(&format!("hit: {}, from recoil", state.hits));
+    }
+    state.last_recoil = recoil;
+
+    let hazard: bool = mem.deref(&gm.hazard_death).unwrap_or_default();
+    if !state.last_hazard && hazard {
+        add_hit(state);
+        asr::print_message(&format!("hit: {}, from hazard", state.hits));
+    }
+    state.last_hazard = hazard;
+
+    let maybe_health: Option<i32> = mem.deref(&pd.health).ok();
+    let game_state: i32 = mem.deref(&gm.game_state).unwrap_or_default();
+    let health_0 = maybe_health == Some(0) && game_state == GAME_STATE_PLAYING;
+    if !state.last_health_0 && health_0 {
+        add_hit(state);
+        asr::print_message(&format!("hit: {}, from heath 0", state.hits));
+    }
+    state.last_health_0 = health_0;
+
+    {
+        if maybe_health != state.last_health {
+            asr::print_message(&format!("health: {:?}", maybe_health));
+        }
+        state.last_health = maybe_health;
+    }
+}
+
+fn add_hit(state: &mut AutoSplitterState) {
+    state.hits += 1;
+    asr::timer::set_variable_int("hits", state.hits);
 }
 
 // --------------------------------------------------------
--- src/silksong_memory.rs
+++ src/silksong_memory.rs
@@ -1,4 +1,10 @@
-use asr::Process;
+use alloc::{boxed::Box, string::String, vec::Vec};
+use asr::{
+    future::next_tick,
+    game_engine::unity::mono::{self, UnityPointer},
+    Address64, Process,
+};
+use bytemuck::CheckedBitPattern;
 
 // --------------------------------------------------------
 
@@ -7,10 +13,201 @@ static SILKSONG_NAMES: [&str; 2] = [
     "Hollow Knight Silksong",     // Mac, Linux
 ];
 
+// const PRE_MENU_INTRO: &str = "Pre_Menu_Intro";
+pub const MENU_TITLE: &str = "Menu_Title";
+pub const QUIT_TO_MENU: &str = "Quit_To_Menu";
+
+// static NON_PLAY_SCENES: [&str; 3] = [PRE_MENU_INTRO, MENU_TITLE, QUIT_TO_MENU];
+
+/*
+static BAD_SCENE_NAMES: [&str; 11] = [
+    "Untagged",
+    "left1",
+    "oncomplete",
+    "Attack Range",
+    "onstart",
+    "position",
+    "looptype",
+    "integer1",
+    "gameObject",
+    "eventTarget",
+    "material",
+];
+*/
+
+pub struct StringListOffsets {
+    string_len: u64,
+    string_contents: u64,
+    /*
+    list_array: u64,
+    array_len: u64,
+    array_contents: u64,
+    */
+}
+
+impl StringListOffsets {
+    fn new() -> StringListOffsets {
+        StringListOffsets {
+            string_len: 0x10,
+            string_contents: 0x14,
+            /*
+            list_array: 0x10,
+            array_len: 0x18,
+            array_contents: 0x20,
+            */
+        }
+    }
+}
+
 // --------------------------------------------------------
 
 pub fn attach_silksong() -> Option<Process> {
     SILKSONG_NAMES.into_iter().find_map(Process::attach)
+}
+
+// --------------------------------------------------------
+
+macro_rules! declare_pointers {
+    ( $g:ident { $( $f:ident : $t:ty = $e:expr ),*, } ) => {
+        pub struct $g {
+            $( pub $f : $t ),*,
+        }
+
+        impl $g {
+            pub fn new() -> $g {
+                $g {
+                    $( $f : $e ),*,
+                }
+            }
+        }
+    };
+}
+
+declare_pointers!(GameManagerPointers {
+    scene_name: UnityPointer<2> = UnityPointer::new("GameManager", 0, &["_instance", "sceneName"]),
+    next_scene_name: UnityPointer<2> = UnityPointer::new("GameManager", 0, &["_instance", "nextSceneName"]),
+    entry_gate_name: UnityPointer<2> = UnityPointer::new("GameManager", 0, &["_instance", "entryGateName"]),
+    game_state: UnityPointer<2> = UnityPointer::new("GameManager", 0, &["_instance", "<GameState>k__BackingField"]),
+    ui_state_vanilla: UnityPointer<3> = UnityPointer::new(
+        "GameManager",
+        0,
+        &["_instance", "<ui>k__BackingField", "uiState"],
+    ),
+    accepting_input: UnityPointer<3> = UnityPointer::new(
+        "GameManager",
+        0,
+        &[
+            "_instance",
+            "<inputHandler>k__BackingField",
+            "acceptingInput",
+        ],
+    ),
+    hazard_death: UnityPointer<4> = UnityPointer::new(
+        "GameManager",
+        0,
+        &[
+            "_instance",
+            "<hero_ctrl>k__BackingField",
+            "cState",
+            "hazardDeath",
+        ],
+    ),
+    hazard_respawning: UnityPointer<4> = UnityPointer::new(
+        "GameManager",
+        0,
+        &[
+            "_instance",
+            "<hero_ctrl>k__BackingField",
+            "cState",
+            "hazardRespawning",
+        ],
+    ),
+    hero_recoil_frozen: UnityPointer<4> = UnityPointer::new(
+        "GameManager",
+        0,
+        &[
+            "_instance",
+            "<hero_ctrl>k__BackingField",
+            "cState",
+            "recoilFrozen",
+        ],
+    ),
+    hero_transition_state: UnityPointer<3> = UnityPointer::new(
+        "GameManager",
+        0,
+        &["_instance", "<hero_ctrl>k__BackingField", "transitionState"],
+    ),
+});
+
+declare_pointers!(PlayerDataPointers {
+    health: UnityPointer<3> = UnityPointer::new("GameManager", 0, &["_instance", "playerData", "health"]),
+});
+
+// --------------------------------------------------------
+
+pub struct Memory<'a> {
+    pub process: &'a Process,
+    pub module: Box<mono::Module>,
+    pub image: mono::Image,
+    pub string_list_offsets: Box<StringListOffsets>,
+}
+
+impl Memory<'_> {
+    pub async fn wait_attach(process: &Process) -> Memory {
+        asr::print_message("Memory wait_attach: Module wait_attach...");
+        next_tick().await;
+        let mut found_module = false;
+        let mut needed_retry = false;
+        loop {
+            let module = mono::Module::wait_attach(&process, mono::Version::V3).await;
+            if !found_module {
+                found_module = true;
+                asr::print_message("Memory wait_attach: module get_default_image...");
+                next_tick().await;
+            }
+            for _ in 0..0x10 {
+                if let Some(image) = module.get_default_image(&process) {
+                    asr::print_message("Memory wait_attach: got module and image");
+                    next_tick().await;
+                    return Memory {
+                        process,
+                        module: Box::new(module),
+                        image,
+                        string_list_offsets: Box::new(StringListOffsets::new()),
+                    };
+                }
+                next_tick().await;
+            }
+            if !needed_retry {
+                needed_retry = true;
+                asr::print_message("Memory wait_attach: retry...");
+                next_tick().await;
+            }
+        }
+    }
+
+    pub fn deref<T: CheckedBitPattern, const CAP: usize>(
+        &self,
+        p: &UnityPointer<CAP>,
+    ) -> Result<T, asr::Error> {
+        p.deref(&self.process, &self.module, &self.image)
+    }
+
+    pub fn read_string<const CAP: usize>(&self, p: &UnityPointer<CAP>) -> Option<String> {
+        let a: Address64 = self.deref(p).ok()?;
+        let n: u32 = self
+            .process
+            .read(a + self.string_list_offsets.string_len)
+            .ok()?;
+        if !(n < 2048) {
+            return None;
+        }
+        let w: Vec<u16> = self
+            .process
+            .read_vec(a + self.string_list_offsets.string_contents, n as usize)
+            .ok()?;
+        String::from_utf16(&w).ok()
+    }
 }
 
 // --------------------------------------------------------
